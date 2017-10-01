dd70132e100305e6780dcaae53cb59db7b29d553
Mayank <mayank.singh081997@gmail.com>
2017-10-01 18:38:17 +0530

    Added inline comments to Pattern1.cpp

--- Patterns/pattern1.cpp
+++ Patterns/pattern1.cpp
@@ -3,24 +3,47 @@ using namespace std;
 
 int main(){
 
+/*take n to be the number of rows*/
+/*declaring the variable n*/
+
 	int n;
+
+/*taking value of n as input from the user*/
+
 	cin >> n;
 
+/*implementing a for loop row by row*/
+/*initializing variable i=0 within for loop*/
+/*total number of times the first for loop will run will be equal to n*/
+
 	for( int i = 0; i < n; i++ ){
+
+/*initializing a variable j=0 in second loop*/
+/*the second loop will run n times*/
+
 		for( int j = 0; j < n; j++ ){
+
+/*priting asterisk (*) without space for the pattern*/
+
 			cout << "*";
 		}
+
+/*once the loop with j variable is complete, cursor will be shifted to new line*/
+
 		cout << endl;
 	}
     return 0;
-
 }
 
 /*
+At n=5, output:
+
 5
 
 *****
 *****
 *****
 *****
+*****
+
 */
