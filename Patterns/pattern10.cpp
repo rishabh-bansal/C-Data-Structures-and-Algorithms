105ca05036ab2848b9bf36eb98e5ca3f6c4ba764
AMAN VERMA <amanv1999@gmail.com>
2017-10-19 17:55:29 +0530

    added a new pattern

---
+++ Patterns/pattern10.cpp
@@ -0,0 +1,16 @@
+    #include <stdio.h>
+  
+    void main() {
+    	int rw, c, no=1 ,len;
+    	clrscr();
+    	printf("Enter number of rows: ");  //asking for the value from user
+    	scanf("%d," &len);                 //taking value from the user
+    	for (rw=1; rw<=len; rw++) {       //iterate the loop from 1 to len
+    		printf("\n");            //for moving into next line
+    		for (c=1; c<=rw; c++) {   //iterating loopfrom 1 to i
+    			printf(" %2d ", no);  //printing number
+    			no++;                 //increasing the number by
+    		}
+    	}
+    	
+    }
