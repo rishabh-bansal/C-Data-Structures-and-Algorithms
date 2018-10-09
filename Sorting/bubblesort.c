b1c117b0a77d5ca0a078246ae2e02ea4b1101d67
taruncode <43909818+taruncode@users.noreply.github.com>
2018-10-09 21:03:22 +0530

    done

--- Sorting/bubblesort.c
+++ Sorting/bubblesort.c
@@ -1,41 +1,41 @@
-#include<stdio.h>
-#define MAX 100
-int main(void)
-{
-	int arr[MAX],i,j,temp,n,xchanges;
-	printf("Enter the number of elements : ");
-	scanf("%d",&n);
-	for(i=0; i<n; i++)
-	{
-		printf("Enter element %d : ",i+1);
-		scanf("%d",&arr[i]);
-	}
-
-	for(i=0; i<n-1 ;i++)
-	{
-		/*
-		xchanges keeps track if any modification has been made in the following 
-		code section . If no swapping was there , it means that the array has 
-		already become sorted and we should break out . */
-		xchanges = 0;
-		for(j=0; j<n-1-i; j++)
-		{
-			if(arr[j] > arr[j+1])
-			{
-				//Swapping the elements
-				temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
-				xchanges++;
-			}
-		}
-		if(xchanges==0) /*If list is sorted*/
-			break;
-	}
-	printf("Sorted list is :\n");
-	for(i=0; i<n; i++)
-		printf("%d ",arr[i]);
-	printf("\n");
-	return 0;
-}
-
+// C program for implementation of Bubble sort 
+#include <stdio.h> 
+  
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
+// A function to implement bubble sort 
+void bubbleSort(int arr[], int n) 
+{ 
+   int i, j; 
+   for (i = 0; i < n-1; i++)       
+  
+       // Last i elements are already in place    
+       for (j = 0; j < n-i-1; j++)  
+           if (arr[j] > arr[j+1]) 
+              swap(&arr[j], &arr[j+1]); 
+} 
+  
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", arr[i]); 
+    printf("n"); 
+} 
+  
+// Driver program to test above functions 
+int main() 
+{ 
+    int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    bubbleSort(arr, n); 
+    printf("Sorted array: \n"); 
+    printArray(arr, n); 
+    return 0; 
+} 
