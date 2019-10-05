2d2f2972fe3424b29ce07dfdbaa4f29bc9f3317a
Deepanshu Chauhan <31282553+cdeepanshu@users.noreply.github.com>
2019-10-05 15:37:09 +0530

    Update Upper Tree Triangle.cpp
    Correct the Output of the code and set the allignment of the code.

--- Patterns/Upper Tree Triangle.cpp
+++ Patterns/Upper Tree Triangle.cpp
@@ -1,25 +1,23 @@
-#include<stdio.h>  			//   including header file (standard input/output)
+#include<stdio.h>  				//   including header file (standard input/output)
 int main()					// main function starts here(begining)
 {
- 
- for(int i=1;i<=5;i++)      // this loop is used to print rows(upto 5)
+  for(int i=1;i<=5;i++)      			// this loop is used to print rows(upto 5)
  {
- 	for(int j=1;j<=9;j++)   // this loop is used to print columns
+ 	for(int j=1;j<=9;j++)   		// this loop is used to print columns
 	 { 
-	 if(j>=6-i && j<=4+i)		//condition to print *
- 		printf("*");
-	else
- 		printf(" ");			// if the above condition doesn't satisfies it prints space
-     }	
+	 	if(j>=6-i && j<=4+i)		//condition to print *
+ 			printf("*");
+		else
+ 			printf(" ");		// if the above condition doesn't satisfies it prints space
+    	 }	
  printf("\n");					/* Used ,Once all the numbers of a row a printed,
-		 							 print a newline before starting the next row.
-		 									*/
- }
- }
+ }		 				 print a newline before starting the next row.
+}		 				*/
+  
   /*  
    OUTPUT:
     
-	*
+    *
    ***
   *****
  *******
