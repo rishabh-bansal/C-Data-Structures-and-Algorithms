a3455c9496063dbeacbd8639f72a21ba9fa528d4
ahtrahdis7 <44672399+ahtrahdis7@users.noreply.github.com>
2019-10-07 12:27:37 +0530

    cleaned the code and added comments

--- LinkedList/circular_LinkedList.c
+++ LinkedList/circular_LinkedList.c
@@ -1,78 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-
-struct node
-{
+//base for individual nodes
+struct node{
 	int data;
 	struct node* link;
 } *start;
-
-void create(int data){
-
+//function to create new node
+void addNode(int data){
 	struct node* new;
-
-	 new = malloc(sizeof(struct node));
-	
+	//creating a new node
+	new = malloc(sizeof(struct node));
+	//condition for checking the existence of previous linkedlist
 	if(start == NULL)
 		start = new;
-
 	new -> data = data;
 	new -> link = start;
-
-
-
+	//creating a dummy for list traversal
 	struct node *q = start;
-
+	//traversing the previously stored linked list
 	while(q -> link != start)
 		q = q -> link;
-
+	//positioning the new node
 	q -> link = new;
-
-
 }
 
-void display()
-{
+void display(){
 	struct node *itr = start;
-	if(start != NULL)
-	{
-		while(itr -> link != start)
-		{
+	if(start != NULL){
+		while(itr -> link != start){
 			printf("%d ", itr -> data );
 			itr = itr -> link;
 		}
 		printf("%d", itr -> data );
-	}
-	else
-	{
+	}else{
 		printf("Nothing to DISPLAY !!\n");
 	}
-
 	printf("\n");
 }
-
-
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
 	start = NULL;
-
 	int n = 0, data = 0;
-
 	printf("Enter the initial number of elements in the linked list:");
-
 	scanf("%d", &n);
 	
-	for (int i = 0; i < n; ++i)
-	{
+	for (int i = 0; i < n; ++i){
 		printf("Enter the value %d:", i + 1);
 		scanf("%d", &data);
-
-		create(data);
+		addNode(data);
 	}
-
 	display();
-
-
 	return 0;
 }
