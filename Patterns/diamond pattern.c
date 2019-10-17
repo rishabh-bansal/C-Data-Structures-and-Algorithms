412c0c4492887553315be4a1b88385199fbf6546
ayush kumar <36650811+Ayushchauha111@users.noreply.github.com>
2019-10-18 04:18:55 +0530

    dimond pattern
    pattern using only two if conditions

---
+++ Patterns/diamond pattern.c
@@ -0,0 +1,71 @@
+/*  program to print following pattern
+5 //user input
+***** *****
+****   ****
+***     ***
+**       **
+*         *
+*         *
+**       **
+***     ***
+****   ****
+***** *****
+
+*/
+
+#include<stdio.h>   //header file for input and output
+#inclusde<conio.h>  //header file for direct run
+int main()   //main function execute from here
+{
+
+    int i,j,l; //decelerations of the variables
+    clrscr();    //for clearing the screen
+    scanf("%d",&l);   //input from user
+    for(i=1;i<=l;i++)  //loop for upper triangle
+    {
+
+  for(j=0;j<=2*l;j++)
+  {
+
+    if((((j>=0)&&(j<=(l-i))))||(((j>=(l+i))&&(j<=2*l))))
+       printf("*"); //printing star
+    else
+        printf(" "); //printing spaces
+  }
+    printf("\n"); //for new line
+   }
+/*
+5
+***** *****
+****   ****
+***     ***
+**       **
+*         *
+*/
+
+    for(i=1;i<=l;i++) //loop for lower triangle
+    {
+
+  for(j=0;j<=2*l;j++)
+  {
+
+    if(((j<=(i-1))&&(j>=0))||((j>=((2*l)-i+1))&&(j<=2*l)))
+       printf("*"); //printing star
+    else
+        printf(" "); //printing space
+  }
+    printf("\n"); //for new line
+   }
+
+
+/*
+5
+*         *
+**       **
+***     ***
+****   ****
+***** *****
+*/
+}
+
+
